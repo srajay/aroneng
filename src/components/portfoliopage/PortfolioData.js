ff40d055c91193cae144689410d4a31b17d8db53
zilong10 <ajaysrchaudhary@gmail.com>
2023-10-19 10:33:22 +0545

    Added portfolio images

--- src/components/portfoliopage/PortfolioData.js
+++ src/components/portfoliopage/PortfolioData.js
@@ -1,126 +1,143 @@
-import Img1 from "../../assets/bg1.jpg";
-import Img2 from "../../assets/bg2.jpeg";
-import Img3 from "../../assets/bg3.jpg";
+import Sunroom from "../../assets/sunroom.png";
+import ADU from "../../assets/adu.png";
+import ShippingContainer from "../../assets/shipping-container.png";
+import InteriorDesign from "../../assets/interiordesign.png";
+import DeckDesign from "../../assets/deckdesign.png";
+import PatioCover from "../../assets/patiocover.jpg";
+import OfficeDesign from "../../assets/officespace.png";
+import PadamPokhari from "../../assets/padampokhari.jpg";
+import Gaumukhi from "../../assets/Gaumukhi Primary Hospital.jpg";
+import BlueHill from "../../assets/Blue Hill Park, Baglung.jpg";
+import FoodMahal from "../../assets/Food Mahal.jpg";
+import Naiet from "../../assets/naiet.jpeg";
+import CulturalMuseum from "../../assets/Multi-Cultural Museum.jpg";
+import Gumba from "../../assets/Kavre Gumba.jpg";
+import TrussHall from "../../assets/halltruss.jpeg";
+import BatukBhairav from "../../assets/Batuk Bhairav Gale Park, Chandragiri.jpg";
+import PadamStadium from "../../assets/Padam Pokhari Stadium, Hetauda.jpg";
+import HyperMart from "../../assets/hyper-mart cafe.jpg";
+import ChandragiriStadium from "../../assets/Chandragiri Stadium.jpg";
+import ShivaStatue from "../../assets/Shiva Statue.jpg";
 
 export const Data = [
   {
-    images: Img1,
+    images: Sunroom,
     title: "3D modelling and rendering for Sunroom Design",
     location: "Location: 75 N Sherman St. Denver CO 80203",
     para: "The scope of this task includes addition of a sunroom to an existing 2-storey building. With a minimal interior design, it integrates with the landscape while sheltering from adverse weather.",
   },
   {
-    images: Img2,
+    images: ADU,
     title: "Accessory Dwelling Unit (ADU) Design",
     location: "Location: 2241 Florence St. Aurora CO",
     para: "The scope of the task included an addition of ADU. The requirement of the client was a family room, kitchen along with a bedroom and wash closet with the proposed opening.",
   },
   {
-    images: Img3,
+    images: ShippingContainer,
     title: "Shipping Container Design",
     location: "Location: Not mentioned",
     para: "The main aim of this project is to create a beautiful yet functional living space for a person or a couple in a shipping container. This container home can also act as a studio apartment.",
   },
   {
-    images: Img1,
+    images: InteriorDesign,
     title: "Interior Design",
     location: "Location: 6449 Spotted Fawn Run, Littleton CO",
     para: "The design intent of this project was to renovate the existing plan into a family room, bedroom, wash closet and guest room along with an art gallery showcase.",
   },
   {
-    images: Img2,
+    images: DeckDesign,
     title: "Deck Design",
     location:
       "Location: Multiple locations for different states in the United States",
     para: "An entire deck design includes engineering of all the structural components in a deck with additional accessories such as fire-place, hot-tub designed with applicable loads and code regulations with proper 2D detailing and 3D rendering for the client.",
   },
   {
-    images: Img3,
+    images: PatioCover,
     title: "Patio Cover, Pergola and Exterior Design",
     location:
       "Location: Multiple locations for different states in the United States",
     para: "We have covered a vast number of designs for patio cover and pergola including the 3D design for exterior spaces.",
   },
   {
-    images: Img1,
+    images: OfficeDesign,
     title: "Office Space Design",
     location: "Location: Aron Engineering Pvt. Ltd., Maitidevi",
     para: "The design scope for this project included the interior design for our office space (Area: 1250 sq. ft.) located in Maitidevi.",
   },
   {
-    images: Img2,
+    images: PadamPokhari,
     title: "Padam Pokhari View Tower",
     location: "Location: Hetauda, Nepal",
     para: "The scope of this task included the design of a view tower to capture the breathtaking view of Padam Pokhari.",
   },
   {
-    images: Img3,
+    images: Gaumukhi,
     title: "Gaumukhi Primary Hospital",
     location: "Location: Pyuthan",
     para: "Design of a class B1 (15 bed) primary hospital with all essential needs. ",
   },
   {
-    images: Img1,
+    images: BlueHill,
     title: "Blue Hill Park",
     location: "Location:  Baglung",
     para: "Master-planning and landscape design for a park in Baglung. ",
   },
   {
-    images: Img2,
+    images: FoodMahal,
     title: "Modern Food Mahal",
     location: "Location: Birtamode",
     para: "Design of a 6 storey complex with multiple grocery stores.",
   },
   {
-    images: Img3,
+    images: Naiet,
     title: "Nepal Army Institute of Engineering Technology",
     location: "Location: Bhaktapur",
     para: "Design of a multi-storey technical college for the project location at the heart of Bhaktapur.",
   },
   {
-    images: Img1,
+    images: CulturalMuseum,
     title: "Multi-cultural Museum",
     location: "Location: Dhangadi",
     para: "Design of a multi-cultural museum with a beautiful landscape.",
   },
   {
-    images: Img2,
+    images: Gumba,
     title: "Gumba Structure",
     location: "Location: Kavre",
     para: "Design of a traditional gumba structure.",
   },
   {
-    images: Img3,
+    images: TrussHall,
     title: "Multi-purpose Truss Hall ",
     location: "Location: Dhangadi (Construction ongoing)",
     para: "Design of a multi-purpose commercial hall with a truss structure.",
   },
   {
-    images: Img1,
+    images: ChandragiriStadium,
     title: "Chandragiri Stadium",
     location: "Location: Chandragiri",
     para: "Design of a football stadium with a basketball court and parking lot.",
   },
   {
-    images: Img2,
+    images: BatukBhairav,
     title: "Batuk Bhairav Gale Park",
     location: "Location: Chandragiri",
     para: "Design and planning of a beautiful recreational park with an-open air theatre (OAT).",
   },
   {
-    images: Img3,
+    images: PadamStadium,
     title: "Padam Pokhari Stadium",
     location: "Location: Hetauda",
     para: "Design of a football stadium at Hetauda.",
   },
   {
-    images: Img1,
+    images: HyperMart,
     title: "Hyper-mart & Café",
     location: "Location:  Kavre",
     para: "Design of a hyper-mart with various shop outlets and a café.",
   },
   {
-    images: Img2,
+    images: ShivaStatue,
     title: "Shiva Statue",
     location: "Location: Baglung",
     para: "Design of Shiva and Ganesh statue as part of the park landscaping project.",
